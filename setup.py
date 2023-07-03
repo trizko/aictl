22c4209a01c804b30bb917d3219b126b0970490d
Tony Rizko <tony@rizkocircle.com>
2023-07-02 20:47:05 -0700

    Update repo to use setuptools for building project

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aictl',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'accelerate',
+        'controlnet_aux',
+        'diffusers',
+        'fastapi',
+        'mediapipe',
+        'opencv-contrib-python',
+        'pdf2image',
+        'Pillow',
+        'pytesseract',
+        'torch',
+        'torchvision',
+        'torchaudio',
+        'transformers',
+        'uvicorn',
+    ],
+    extras_require={
+        'xformers':  [
+            'xformers',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'aictl = aictl.main:main',
+        ],
+    },
+)
