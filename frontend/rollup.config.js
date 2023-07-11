6a2ea8d90ba8eaeba72edab2a3f9c3318698576d
Tony Rizko <tony@rizkocircle.com>
2023-07-10 19:25:03 -0700

    Initialize svelte frontend app with rollup bundler

---
+++ frontend/rollup.config.js
@@ -0,0 +1,65 @@
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import livereload from 'rollup-plugin-livereload';
+import css from 'rollup-plugin-css-only';
+import terser from '@rollup/plugin-terser';
+import { spawn } from 'child_process';
+
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+    return {
+        writeBundle() {
+          if (server) return;
+          // Spawn a child server process
+          server = spawn(
+            'npm',
+            ['run', 'start', '--', '--dev'],
+            {
+              stdio: ['ignore', 'inherit', 'inherit'],
+              shell: true,
+            }
+          );
+    
+          // Kill server on process termination or exit
+          process.on('SIGTERM', toExit);
+          process.on('exit', toExit);
+        },
+      };
+}
+
+export default {
+	input: 'src/main.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: 'public/build/bundle.js'
+	},
+	plugins: [
+		svelte({
+			compilerOptions: {
+				dev: !production
+			}
+		}),
+		css({ output: 'bundle.css' }),
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+		!production && serve(),
+		!production && livereload('public'),
+        production && terser(),
+	],
+	watch: {
+		clearScreen: false
+	}
+};
---
+++ frontend/src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    // You can add your JavaScript code here
+</script>
+
+<style>
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        background: #000;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
+        grid-auto-rows: 50px;
+        grid-gap: 5px;
+    }
+
+    .grid-item {
+        background: rgba(255, 255, 255, 0.1);
+    }
+</style>
+
+<main>
+    {#each Array(100).fill() as _}
+        <div class="grid-item"></div>
+    {/each}
+</main>
