4031b96b365f9911111e3c25600fee3a4e2cd4dc
Tony Rizko <tony@rizkocircle.com>
2023-07-13 19:37:23 -0700

    Update Toolbar component styling

--- frontend/src/components/Toolbar.svelte
+++ frontend/src/components/Toolbar.svelte
@@ -8,6 +8,6 @@
     @tailwind utilities;
 </style>
 
-<div class="bg-gray-500 px-2 py-2 ml-2 fixed inline-block">
+<div class="bg-gray-500 px-2 py-2 ml-2 fixed inline-block rounded">
     <input class="py-1 px-1" type="text">
 </div>
