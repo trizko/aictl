7060d604b1e79cc36c7df3b87d40c90100c694c6
Tony Rizko <tony@rizkocircle.com>
2023-07-23 15:27:20 -0700

    Add dark/light mode toggle

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./public/index.html', './src/**/*.svelte'],
+  content: ['./public/index.html', './src/**/*.svelte', './src/global.css'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
