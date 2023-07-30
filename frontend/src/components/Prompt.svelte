bfa03e35a26a319f2eea2bd6fe9f430e4aa92da9
Tony Rizko <tony@rizkocircle.com>
2023-07-30 03:50:16 -0700

    Update Prompt component to make request to backend on button press

--- frontend/src/components/Prompt.svelte
+++ frontend/src/components/Prompt.svelte
@@ -1,13 +1,26 @@
 <script>
+    import { dataStore } from '../store.js';
+    let prompt = '';
+    let result;
+
     async function generateImage() {
-        const response = await fetch("http://localhost:8000/generate");
+        const response = await fetch("http://localhost:8000/generate", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt }),
+        });
+
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        data = await response.json();
 
-        return data
+        result = await response.json();
+        dataStore.set(result.path);
     }
+
+    generateImage();
 </script>
 
 <style type="text/postcss">
@@ -17,6 +30,6 @@
 </style>
 
 <div class="bg-gray-500 px-2 py-2">
-    <input class="py-1 px-1" type="text" />
+    <input bind:value={prompt} class="py-1 px-1" type="text" />
 </div>
 <button on:click={generateImage} class="text-gray-900 bg-orange-400 rounded-r text-xl font-black px-5 py-1 hover:bg-orange-300">generate</button>
